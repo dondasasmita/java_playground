94819b8fdbbf307d6c5a850f0bf0c5b5f5bba487
dondasasmita <dondasasmita@gmail.com>
2021-04-25 16:04:51 +0800

    removed test file

--- src/greeting/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package greeting;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World 2");
-    }
-}
